cffd354e1789f9ef6f6761ed8729e86782579b3d
anisen <ananavelez@gmail.com>
2017-04-27 12:57:58 -0300

    iniciar

---
+++ js/proyecto-final.js
@@ -0,0 +1,76 @@
+var arr=[
+  {
+    "userId": 1,
+    "id": 1,
+    "title": "delectus aut autem",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 2,
+    "title": "quis ut nam facilis et officia qui",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 3,
+    "title": "fugiat veniam minus",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 4,
+    "title": "et porro tempora",
+    "completed": true
+  },
+  {
+    "userId": 1,
+    "id": 5,
+    "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 6,
+    "title": "qui ullam ratione quibusdam voluptatem quia omnis",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 7,
+    "title": "illo expedita consequatur quia in",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 8,
+    "title": "quo adipisci enim quam ut ab",
+    "completed": true
+  },
+  {
+    "userId": 1,
+    "id": 9,
+    "title": "molestiae perspiciatis ipsa",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 10,
+    "title": "illo est ratione doloremque quia maiores aut",
+    "completed": true
+  }
+  ];
+var mostrar= [];
+ for (i=0;i<10;i++){
+ mostrar.push(arr[i])
+};
+
+mostrar.forEach(function(x){
+  document.write("<li>ID: "+x.id + "<br>"+"<blockquote>Titulo: " +x.title+"<br>"+"Booleano: "+x.completed +"<br>"+ "</li>");
+  });
+
+function nombreFuncion(){
+var info= document.getElementsByClassName("informacion");
+var tarea=document.getElementById("tareaInput").value;
+info[0].innerHTML +="<li>"+tarea+"</li>";
+}
